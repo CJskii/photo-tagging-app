fa39a22ace07a0512238d7031698a44e55e97338
CJskii <czareczarek3@gmail.com>
2023-03-03 16:33:09 +0000

    Navbar + Themes

---
+++ my-app/src/Components/Navbar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Themes from "./Themes";
+
+interface NavbarProps {
+  changeTheme: (event: React.MouseEvent<HTMLSpanElement>) => void;
+}
+
+const Navbar = ({ changeTheme }: NavbarProps) => {
+  return (
+    <div
+      data-testid="navbar"
+      className="navbar bg-base-200"
+      style={{ height: "8vh" }}
+    >
+      <div className="navbar-start lg:flex">
+        <Link className="btn btn-ghost normal-case text-xl" to="/">
+          Waldo
+        </Link>
+      </div>
+
+      <div className="navbar-end">
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li className="mx-4">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="mx-4">
+              <Link to="/products">Game</Link>
+            </li>
+            <li className="mx-4">
+              <Link to="/contact">Leaderboard</Link>
+            </li>
+          </ul>
+        </div>
+        <Themes changeTheme={changeTheme} />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- my-app/tailwind.config.cjs
+++ my-app/tailwind.config.cjs
@@ -5,4 +5,23 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        default: {
+          primary: "#12a8cf",
+          secondary: "#c33829",
+          accent: "#17c3f1",
+          neutral: "#8f8d85",
+          "base-100": "#12a8cf",
+          "base-200": "#c33829",
+          "base-content": "#f0f0f0",
+          "primary-content": "#000000",
+          "secondary-content": "#8f8d85",
+        },
+      },
+      "dark",
+      "synthwave",
+    ],
+  },
 };
