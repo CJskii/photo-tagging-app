cf87c98857fb7d91529d12a9b604bc30f20cb6c2
CJskii <czareczarek3@gmail.com>
2023-03-07 01:13:53 +0000

    Level1 adjustments

--- my-app/src/Components/Levels/Level1.tsx
+++ my-app/src/Components/Levels/Level1.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import PreviewIcons from "./PreviewIcons";
+import Time from "../Time";
+import RestartBtn from "../RestartBtn";
 
 interface Props {
   name: string;
@@ -18,15 +20,26 @@ const Level1 = () => {
   ]);
 
   return (
-    <div className="w-full h-full flex flex-col justify-start items-center gap-24 py-12">
-      <h1 className="text-5xl">Level 1</h1>
-      <div className="flex justify-center items-center gap-4">
-        {characters.map((char: Props, index: number) => {
-          if (char.render) {
-            return <PreviewIcons char={char} key={index} />;
-          }
-        })}
+    <div className="w-full h-full flex flex-col justify-start items-center gap-4 py-8 bg-primary">
+      <h1 className="text-5xl lg:pr-8">Level 1</h1>
+      <div className="flex justify-between items-center lg:w-[90vw]">
+        <div className="flex flex-col justify-center items-start gap-4 w-content px-4">
+          <h2 className="font-bold text-2xl">Characters to find: </h2>
+          <div className="flex justify-center items-center gap-4 w-content">
+            {characters.map((char: Props, index: number) => {
+              if (char.render) {
+                return <PreviewIcons char={char} key={index} />;
+              }
+            })}
+          </div>
+        </div>
+        <RestartBtn />
+        <Time />
       </div>
+      <img
+        src="levels/level-1.jpg"
+        className="rounded lg:w-[90vw] max-lg:p-2"
+      />
     </div>
   );
 };
--- my-app/src/Components/Levels/PreviewIcons.tsx
+++ my-app/src/Components/Levels/PreviewIcons.tsx
@@ -9,11 +9,14 @@ interface Props {
 
 const PreviewIcons = (char: Props) => {
   return (
-    <img
-      className="w-[50px] h-[50px]"
-      src={`/icons/${char.char.name}-transparent.png`}
-      alt={`${char.char.name} transparent`}
-    />
+    <div className="flex flex-col justify-center items-center">
+      <img
+        className="w-[50px] h-[50px]"
+        src={`/icons/${char.char.name}-transparent.png`}
+        alt={`${char.char.name} transparent`}
+      />
+      <p>{char.char.name.charAt(0).toUpperCase() + char.char.name.slice(1)}</p>
+    </div>
   );
 };
 
