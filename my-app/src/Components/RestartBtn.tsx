ce26bcb5d6c904e4ca23da600f35d702a54b36f7
CJskii <czareczarek3@gmail.com>
2023-03-07 01:13:37 +0000

    RestartBtn + Time Components

---
+++ my-app/src/Components/RestartBtn.tsx
@@ -0,0 +1,9 @@
+const RestartBtn = () => {
+  return (
+    <button className="btn bg-base-200 hover:bg-secondary hover:animate-bounce">
+      Restart
+    </button>
+  );
+};
+
+export default RestartBtn;
---
+++ my-app/src/Components/Time.tsx
@@ -0,0 +1,11 @@
+const Time = () => {
+  return (
+    <div className="flex flex-col justify-center items-end px-4">
+      <div className="text-xl font-bold">📢 Current time: 00:00:55 </div>
+      <div className="text-xl">🌟 Your best time: 00:06:08</div>
+      <div className="text-xl">🏆 Leaderboard top: 00:01:01</div>
+    </div>
+  );
+};
+
+export default Time;
