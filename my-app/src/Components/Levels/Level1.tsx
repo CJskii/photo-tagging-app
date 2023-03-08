3d1a6e8d2a372678ae7ac18b3dc833d3b77cf2a9
CJskii <czareczarek3@gmail.com>
2023-03-08 19:35:11 +0000

    Selection box

--- my-app/src/Components/Levels/Level1.tsx
+++ my-app/src/Components/Levels/Level1.tsx
@@ -1,3 +1,4 @@
+import Selection from "./Selection";
 import React, { useState, useRef, useEffect } from "react";
 import PreviewIcons from "./PreviewIcons";
 import Time from "../Time";
@@ -45,7 +46,6 @@ const Level1 = () => {
         `Clicked at (${pixelX}, ${pixelY}) in image with size (${imageWidth}, ${imageHeight})`
       );
       return { x: pixelX, y: pixelY };
-      // validate
     }
   };
 
@@ -73,22 +73,7 @@ const Level1 = () => {
             style={{ top: `${clickCoords.y}px`, left: `${clickCoords.x}px` }}
           >
             {characters.map((char, index) => {
-              if (char.render)
-                return (
-                  <div
-                    key={index}
-                    className="flex justify-center items-center border-2 gap-2 rounded bg-base-200 hover:bg-secondary cursor-pointer my-2"
-                  >
-                    <img
-                      className="w-[50px] h-[50px] m-2"
-                      src={`/icons/${char.name}-transparent.png`}
-                      alt={`${char.name} transparent`}
-                    />
-                    <div className="pr-4">
-                      {char.name.charAt(0).toUpperCase() + char.name.slice(1)}
-                    </div>
-                  </div>
-                );
+              if (char.render) return <Selection key={index} {...char} />;
             })}
           </div>
         )}
---
+++ my-app/src/Components/Levels/Selection.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface Props {
+  name: string;
+  render: boolean;
+}
+
+const Selection = (char: Props) => {
+  console.log(char);
+  return (
+    <>
+      <div className="flex justify-center items-center border-2 gap-2 rounded bg-base-200 hover:bg-secondary cursor-pointer my-2">
+        <img
+          className="w-[50px] h-[50px] m-2"
+          src={`/icons/${char.name}-transparent.png`}
+          alt={`${char.name} transparent`}
+        />
+        <div className="pr-4">
+          {char.name.charAt(0).toUpperCase() + char.name.slice(1)}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Selection;
