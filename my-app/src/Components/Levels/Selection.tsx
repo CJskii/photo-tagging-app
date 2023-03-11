b67671cbdd5b18bd66c787f251bb3d2568cb5b3f
CJskii <czareczarek3@gmail.com>
2023-03-11 22:52:46 +0000

    Conditional rendering + updateCharacter function

--- my-app/src/Components/Levels/Selection.tsx
+++ my-app/src/Components/Levels/Selection.tsx
@@ -1,39 +1,54 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface Props {
   name: string;
   render: boolean;
+  isFound: boolean;
   imageClickCoords: object;
   setShowSelectionBox: (showSelectionBox: boolean) => void;
+  updateCharacter: (obj: object) => void;
 }
 
 const Selection = (char: Props) => {
+  const [characterFound, setCharacterFound] = useState(char.isFound);
+
+  useEffect(() => {
+    setCharacterFound(char.isFound);
+  }, [char.isFound]);
+
   const ref = useRef<HTMLDivElement>(null);
 
   const handleClick = () => {
+    //console.log(char);
     let userSelection = ref.current?.textContent;
     const selectedCoords = char.imageClickCoords;
     char.setShowSelectionBox(false);
+
     console.log({ userSelection, selectedCoords });
     // pass userSelection and selectedCoords to backend for validation
+
+    // if click is found - update character data
+    char.updateCharacter(char);
   };
 
   return (
     <>
-      <div
-        className="flex justify-center items-center border-2 gap-2 rounded bg-base-200 hover:bg-secondary cursor-pointer my-2"
-        onClick={handleClick}
-        id={`${char.name}`}
-      >
-        <img
-          className="w-[50px] h-[50px] m-2"
-          src={`/icons/${char.name}-transparent.png`}
-          alt={`${char.name} transparent`}
-        />
-        <div className="pr-4" ref={ref}>
-          {char.name.charAt(0).toUpperCase() + char.name.slice(1)}
+      {characterFound ? null : (
+        <div
+          className="flex justify-center items-center border-2 gap-2 rounded bg-base-200 hover:bg-secondary cursor-pointer my-2"
+          onClick={handleClick}
+          id={`${char.name}`}
+        >
+          <img
+            className="w-[50px] h-[50px] m-2"
+            src={`/icons/${char.name}-transparent.png`}
+            alt={`${char.name} transparent`}
+          />
+          <div className="pr-4" ref={ref}>
+            {char.name.charAt(0).toUpperCase() + char.name.slice(1)}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
