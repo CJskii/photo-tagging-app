ae5cc39ec8948edebd400ce2b03fb6972f5346f0
CJskii <czareczarek3@gmail.com>
2023-03-11 22:51:26 +0000

    render icons conditionally

--- my-app/src/Components/Levels/PreviewIcons.tsx
+++ my-app/src/Components/Levels/PreviewIcons.tsx
@@ -1,6 +1,7 @@
 interface CharData {
   name: string;
   render: boolean;
+  isFound: boolean;
 }
 
 interface Props {
@@ -8,8 +9,17 @@ interface Props {
 }
 
 const PreviewIcons = (char: Props) => {
+  const isFound = () => {
+    if (char.char.isFound) return "opacity-40";
+  };
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div
+      className={`flex flex-col justify-center items-center relative ${isFound()}`}
+    >
+      {char.char.isFound ? (
+        <div className="w-[10px] h-[60px] bg-lime-600 rotate-45 absolute"></div>
+      ) : null}
+
       <img
         className="w-[50px] h-[50px]"
         src={`/icons/${char.char.name}-transparent.png`}
