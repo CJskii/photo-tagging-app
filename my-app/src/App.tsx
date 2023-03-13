570a270307d264a5f4fbd842cf7c215628194ca2
CJskii <czareczarek3@gmail.com>
2023-03-13 00:45:46 +0000

    userName useContext

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useContext, createContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
@@ -7,8 +7,13 @@ import Game from "./Components/Game";
 import Leaderboard from "./Components/Leaderboard";
 import Level1 from "./Components/Levels/Level1";
 
+export const UsernameContext = createContext<
+  [string, React.Dispatch<React.SetStateAction<string>>]
+>(["", () => {}]);
+
 function App() {
   const [theme, setTheme] = useState("default");
+  const [userName, setUserName] = useState("");
 
   const changeTheme = (event: React.MouseEvent<HTMLSpanElement>) => {
     const selected = event.currentTarget.textContent?.toLowerCase();
@@ -19,17 +24,19 @@ function App() {
     <div className="App bg-base-100 min-h-screen" data-theme={theme}>
       <Navbar changeTheme={changeTheme} />
       <section className="min-h-[92vh] home">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/game" element={<Game />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/level1" element={<Level1 />} />
-          {/* <Route path="/level2" element={<Level2 />} />
+        <UsernameContext.Provider value={[userName, setUserName]}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/level1" element={<Level1 />} />
+            {/* <Route path="/level2" element={<Level2 />} />
           <Route path="/level3" element={<Level3 />} />
           <Route path="/level4" element={<Level4 />} />
           <Route path="/level5" element={<Level5 />} />
           <Route path="/level6" element={<Level6 />} /> */}
-        </Routes>
+          </Routes>
+        </UsernameContext.Provider>
       </section>
       <Footer />
     </div>
--- my-app/src/Components/Levels/GameEnded.tsx
+++ my-app/src/Components/Levels/GameEnded.tsx
@@ -1,14 +1,26 @@
 import RestartBtn from "../RestartBtn";
 import { Link } from "react-router-dom";
-import { useRef, useState } from "react";
+import { useRef, useContext } from "react";
+import { UsernameContext } from "../../App";
+
+interface Props {
+  userName: string | null;
+  setUserName: (user: string) => void;
+}
 
 const GameEnded = () => {
+  const [userName, setUserName] = useContext(UsernameContext);
   const inputRef = useRef<HTMLInputElement>(null);
   // Get time
   // Get username
 
   const submitToLeaderboard = () => {
-    console.log(inputRef.current?.value);
+    if (inputRef.current?.value.length) {
+      setUserName(inputRef.current.value);
+    }
+
+    console.log(userName);
+
     // validate if user is already in leaderboard
     // if !leaderboard create new record
     // if user is already in leaderboard check if this time is better
@@ -17,7 +29,9 @@ const GameEnded = () => {
 
   return (
     <div className="h-full flex flex-col justify-center items-center gap-2">
-      <h1 className="text-5xl text-center mb-8">Congratulations! 🥳</h1>
+      <h1 className="text-5xl text-center mb-8">
+        Congratulations {userName}! 🥳
+      </h1>
       <p className="text-2xl"> You have found all characters 💪</p>
       <p className="text-2xl py-4">Your time is: </p>
       <p className="text-xl">
@@ -33,15 +47,19 @@ const GameEnded = () => {
         </Link>
         to select a different level
       </p>
-      <label className="text-2xl pt-8">Enter your name: </label>
-      <input
-        type="text"
-        className="w-[250px] text-xl pb-2 mb-4"
-        ref={inputRef}
-      />
+      {!userName ? (
+        <>
+          <label className="text-2xl pt-8">Enter your name: </label>
+          <input
+            type="text"
+            className="w-[250px] text-xl pb-2 mb-4"
+            ref={inputRef}
+          />
+        </>
+      ) : null}
 
       <button
-        className="btn bg-base-200 hover:bg-secondary hover:animate-bounce"
+        className="btn bg-base-200 hover:bg-secondary hover:animate-bounce mt-4"
         onClick={submitToLeaderboard}
       >
         Submit
